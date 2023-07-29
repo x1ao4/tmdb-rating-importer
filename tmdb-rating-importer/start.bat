09643304762b0c776739f48acd7fd2585a90c46a
x1ao4 <kazaf_ken@163.com>
2023-07-29 15:21:31 +0800

    Initial commit

---
+++ tmdb-rating-importer/start.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd C:\path\to\tmdb-rating-importer
+
+python tmdb-rating-importer.py
+
+pause
---
+++ tmdb-rating-importer/start.command
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /path/to/tmdb-rating-importer
+
+python3 tmdb-rating-importer.py
